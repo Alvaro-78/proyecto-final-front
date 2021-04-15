4842072bc2bea568966dd0d5d0a16e8871398dcd
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-04-15 17:08:30 +0200

    Create react scaffolding

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Proyecto final e-commer
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
