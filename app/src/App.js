a212872993615dd1acc73bc63d7c7c5ed515961b
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-04-28 07:36:01 +0200

    Add container path to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,22 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Home from '../src/containers/home/home'
+import Login from '../src/containers/login/login'
+
+
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Proyecto final e-commer
-        </p>
-      </header>
-    </div>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                <Route path='/' exact component={Home}/>
+                <Route path='/login' exact component={Login}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
   );
 }
 
