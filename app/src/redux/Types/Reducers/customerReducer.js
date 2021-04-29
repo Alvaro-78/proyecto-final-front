536f4f055ab16b3c740d819b098fff326c400c5c
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-04-29 16:45:11 +0200

    Add customerReducer

---
+++ app/src/redux/Types/Reducers/customerReducer.js
@@ -0,0 +1,34 @@
+
+import {LOGIN, LOGOUT} from '../Types/customerType';
+
+const initialState = {
+  customer : {},
+  token : "" 
+};
+
+
+const customerReducer = (state = initialState, action) => {
+
+  switch(action.type){
+
+    case LOGIN : 
+      return{
+        ...state,
+        customer : action.payload.customer,
+        token : action.payload.token
+      };
+
+    case LOGOUT :
+      return {
+        ...state,
+        customer : initialState
+      }
+
+    default : 
+      return state
+
+  };
+};
+
+
+export default customerReducer;
