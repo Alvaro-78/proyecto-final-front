45e8e999ba9f3c6f03ed34c1e4918abefce2604c
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-04-29 13:29:27 +0200

    Add login folder

--- app/src/containers/login/login.jsx
+++ app/src/containers/login/login.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
 import './login.css';
 
-const login = () => {
+const Login = () => {
     return (
-        <div>
+        <div className="loginBody">
             <div className="header">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="/">DollMinatrix</a>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">DollMinatrix</a>
                     </div>
                 </nav>
                 <div className="spaceUnderHeader"></div>
             </div>
             <div className="loginContainer">
-                <div className="spaceLeft"></div>
                 <form className="formControl">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                        <div id="emailHelp" className="form-text">Nos preocupa tu intimidad, tus datos nunca serán compartidos o vendidos.</div>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                    <div className="mb-3">
+                        <label for="inputPassword5" className="form-label">Password</label>
+                        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock"></input>
+                        <div id="passwordHelpBlock" className="form-text">
+                        Su contraseña debe tener entre 8 y 20 caracteres, contener letras y números, y no debe contener espacios, caracteres especiales ni emoji
+                        </div>
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Enviar</button>
                 </form>
             </div>
         </div>
     )
 }
 
-export default login
+export default Login
