f2db2ecaebe40c48ecc51944906ace5b7bfb57dc
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 21:55:42 +0200

    Add accesories container

---
+++ app/src/containers/Categories/Accesories/Accesories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Accesories.css'
+
+const accesories = () => {
+    return (
+        <div>
+            <h1>Hola que ase???</h1>
+        </div>
+    )
+}
+
+export default accesories
