797d5477ac66404a1c0ba1e6eed8062c43b11f18
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 12:57:10 +0200

    Add custom navbar

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,73 +1,112 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { LOGOUT } from '../../redux/Types/customerType.js';
-// import {useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
+import './Navbar.css'
 
 
 
 
 const Navbar = (props) => {
 
+    let history = useHistory();
+
     const logOut =  () => {
+
+        /*eslint no-restricted-globals: ["error", "event"]*/
 
         props.dispatch({ type: LOGOUT, payload : {}});
     
-        // setTimeout(()=> {
-        //     history.push('/');
-        // },300);
+        setTimeout(()=> {
+            history.push('/');
+        },300);
     };
-    console.log(props.costumer)
 
-    if(props.costumer?.id) {
+    console.log(props.customer?.id)
+
+    if(props.customer?.id) {
 
         return(
-
-            <nav className="navbar navbar-expand-lg navbar-grey bg-grey">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">DOLLMINATRIX</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                        
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <div className="headerNavbar">
+                <nav className="navbar navbar-expand-lg navbar-grey bg-grey">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">DOLLMINATRIX</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                        
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/login">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/register">Register</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Categories
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+                                    <a className="nav-link active" aria-current="page" href="/login">Profile</a>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Categories
+                                    </a>
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a className="dropdown-item" href="#">Action</a></li>
+                                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active logOut"  aria-current="page" onClick={() => logOut()}>Salir</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                    <div className="customerName">
+                        ¡Hola {props.customer?.name}!
+                    </div>
+                </nav>
+            </div>    
         )    
     } else {
 
+        return (
+            <div className="headerNavbar">
+                <nav className="navbar navbar-expand-lg navbar-grey bg-grey">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">DOLLMINATRIX</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                        
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="/register">Register</a>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Categories
+                                    </a>
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a className="dropdown-item" href="#">Action</a></li>
+                                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>    
+        )
     }
 
-    return (
-        <div>
-        </div>
-    )
 }
 
-const mapsStateToProps = state => {
+const mapStateToProps = state => {
 
     return {
-        customer : state.custommerReducer.customer,
+        customer : state.customerReducer.customer,
         token : state.customerReducer.token
     }
  };
 
-export default connect(mapsStateToProps)(Navbar);
+export default connect(mapStateToProps)(Navbar);
