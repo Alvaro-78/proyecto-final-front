949b4c96bd664c371ab2196258907d7faeffd223
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 21:57:00 +0200

    Add Unisex container

---
+++ app/src/containers/Categories/Unisex/Unisex.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Unisex.css';
+
+const Unisex = () => {
+    return (
+        <div>
+            <h1>Unisex</h1>
+        </div>
+    )
+}
+
+export default Unisex
