956024b744f009f65fde192c08f534a8d4fbb968
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 21:56:48 +0200

    Add Shoes container

---
+++ app/src/containers/Categories/Shoes/Shoes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shoes.css';
+
+const Shoes = () => {
+    return (
+        <div>
+            <h1>Calzado</h1>
+        </div>
+    )
+}
+
+export default Shoes
