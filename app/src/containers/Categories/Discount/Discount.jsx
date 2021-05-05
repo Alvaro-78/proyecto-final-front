fa04a05f072e373ca9ab55ccbf1e70a027c73fc1
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 21:56:06 +0200

    Add discount container

---
+++ app/src/containers/Categories/Discount/Discount.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Discount.css';
+
+const Discount = () => {
+    return (
+        <div>
+            <h1>Descuentos</h1>
+        </div>
+    )
+}
+
+export default Discount
