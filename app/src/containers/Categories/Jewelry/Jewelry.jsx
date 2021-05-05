c80beaf5c81394baca07689928a43c28ef181299
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 21:56:34 +0200

    Add Jewelry

---
+++ app/src/containers/Categories/Jewelry/Jewelry.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Jewelry.css'
+
+const Jewelry = () => {
+    return (
+        <div>
+            <h1>Jewelry</h1>
+        </div>
+    )
+}
+
+export default Jewelry
