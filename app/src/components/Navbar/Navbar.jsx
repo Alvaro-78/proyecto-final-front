a4b72624653d65956e203acd899a07ad8c1080af
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-05 11:49:01 +0200

    Add user login view to navbar component

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,73 @@
-import React from 'react'
+import React from 'react';
+import {connect} from 'react-redux';
+import { LOGOUT } from '../../redux/Types/customerType.js';
+// import {useHistory} from 'react-router-dom';
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-grey bg-grey">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="'/">DOLLMINATRIX</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+
+
+
+const Navbar = (props) => {
+
+    const logOut =  () => {
+
+        props.dispatch({ type: LOGOUT, payload : {}});
+    
+        // setTimeout(()=> {
+        //     history.push('/');
+        // },300);
+    };
+    console.log(props.costumer)
+
+    if(props.costumer?.id) {
+
+        return(
+
+            <nav className="navbar navbar-expand-lg navbar-grey bg-grey">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">DOLLMINATRIX</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                        
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/login">Login</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/login">Login</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="/register">Register</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/register">Register</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Categories
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider"/></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
                             </li>
-                            {/* <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li> */}
                         </ul>
-                        {/* <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
                     </div>
                 </div>
             </nav>
+        )    
+    } else {
+
+    }
+
+    return (
+        <div>
         </div>
     )
 }
 
-export default Navbar
+const mapsStateToProps = state => {
+
+    return {
+        customer : state.custommerReducer.customer,
+        token : state.customerReducer.token
+    }
+ };
+
+export default connect(mapsStateToProps)(Navbar);
--- app/src/containers/Home/Home.jsx
+++ app/src/containers/Home/Home.jsx
@@ -9,19 +9,6 @@ const Home = () => {
         <div className="homeContainer">
             <div className="header">
                 <Navbar/>
-                {/* <a className="logoName" href="/">DOLLMINATRIX</a>
-                <a className="loginName" href="/login">Login</a>
-                <a className="registerName" href="/register">Register</a>
-                <div className="dropdown">
-                <button className="dropdownButton dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Categories
-                </button>
-                <ul className="dropdown-menu">
-                    <li><a  href="#">Action</a></li>
-                    <li><a  href="#">Another action</a></li>
-                    <li><a  href="#">Something else here</a></li>
-                </ul>
-                </div> */}
             </div>
             <div className="imageDiv">
                 <img className="imgMovie" alt="dolls" src={portadamuñecas}></img>
