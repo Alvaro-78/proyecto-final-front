d96d90ea50abf4ce485c95b520442e521559cadf
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 22:37:24 +0200

    Add logic to Jewelry

--- app/src/containers/Categories/Jewelry/Jewelry.jsx
+++ app/src/containers/Categories/Jewelry/Jewelry.jsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import './Jewelry.css'
+import React, {useState , useEffect } from 'react';
+import './Jewelry.css';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import {SHOW} from '../../../redux/Types/productType';
+import Navbar from '../../../components/Navbar/Navbar';
 
-const Jewelry = () => {
+
+
+const Jewelry = (props) => {
+
+    useEffect(()=>{
+        
+        getImg();
+        // eslint-disable-next-line
+    },[])
+
+    const [products, setProducts] = useState ({
+
+        listProducts : []
+    })
+
+    const getImg = async () => {
+
+        const imgCollection = await axios.get('http://localhost:3000/products')
+
+        setProducts({
+            ...products, listProducts: imgCollection.data
+        })
+    }
+
+    const jewelry = products.listProducts.filter(objecProduct => objecProduct.category.includes('Jewelry'));
+
+    const saveProduct = () => {
+        props.dispatch({type: SHOW,  saveProduct})
+    }
+
     return (
         <div>
-            <h1>Jewelry</h1>
+        <Navbar/>
+        <div className="spaceUnderNav"></div>
+        <div className="boyContainer"></div>
+            <div className="imgJewelryContainer" onClick={() => saveProduct()}>
+                {
+                    jewelry.map(productTitle => {
+                        return(
+                            <div className="productsData" key={products._id}>
+                                <div>
+                                    <img className="imgBoy" src={productTitle.picture}></img>
+                                    <div className="imgTitle">{productTitle.name}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
-    )
+ )
+
 }
 
-export default Jewelry
+const mapStateToProps = state => {
+    return {
+        showProduct : state.productReducer.showProduct
+    }
+};
+
+export default connect(mapStateToProps)(Jewelry)
