4d4a4c60af66d968d3bc86257ad284eb07557e7e
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 22:37:05 +0200

    Add Girl container login

--- app/src/containers/Categories/Girl/Girl.jsx
+++ app/src/containers/Categories/Girl/Girl.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, {useState , useEffect } from 'react';
 import './Girl.css';
 import Navbar from '../../../components/Navbar/Navbar';
-import girl1 from '../../../assets/Girl-Pics/girl1.jpeg'
-import girl2 from '../../../assets/Girl-Pics/girl2.jpeg'
-import girl3 from '../../../assets/Girl-Pics/girl3.jpeg'
-import girl4 from '../../../assets/Girl-Pics/girl4.jpeg'
+import axios from 'axios';
+import { connect } from 'react-redux';
+import {SHOW} from '../../../redux/Types/productType'
 
-const Girl = () => {
+
+const Girl = (props) => {
+
+    useEffect(()=>{
+        
+        getImg();
+        // eslint-disable-next-line
+    },[])
+
+    const [products, setProducts] = useState ({
+
+        listProducts : []
+    })
+
+    const getImg = async () => {
+
+        const imgCollection = await axios.get('http://localhost:3000/products')
+
+        setProducts({
+            ...products, listProducts: imgCollection.data
+        })
+    }
+
+    const girl = products.listProducts.filter(objecProduct => objecProduct.category.includes('Girl'));
+
+    const saveProduct = () => {
+        props.dispatch({type: SHOW,  saveProduct})
+    }
+
+    
+
     return (
         <div className="girlBody">
             <Navbar/>
             <div className="spaceUnderNav"></div>
-            <div className="imgGirlContainer">
-                <img className="girlPic" alt="chico1" src={girl1}></img>
-                <img className="girlPic" alt="chico1" src={girl2}></img>
-                <img className="girlPic" alt="chico1" src={girl3}></img>
-                <img className="girlPic" alt="chico1" src={girl4}></img>
+            <div className="boyContainer">
+                <div className="imgBoyContainer">
+                {
+                    girl.map(productTitle => {
+                        return(
+                            <div className="productsData" key={products._id}>
+                                <div>
+                                        <img className="imgBoy" src={productTitle.picture}></img>
+                                        <div className="imgTitle">{productTitle.name}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                </div>
             </div>
         </div>
     )
