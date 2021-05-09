061d5cdd1bc4d9881efe8f3ba21bb4212f402464
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 22:36:51 +0200

    Create Boy login

--- app/src/containers/Categories/Boy/Boy.jsx
+++ app/src/containers/Categories/Boy/Boy.jsx
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, {useState , useEffect } from 'react';
 import Navbar from '../../../components/Navbar/Navbar';
 import './Boy.css';
-// import chico1 from '../../../../public/uploads/Boy-Pics/chico1.jpeg';
-// import chico2 from '../../../../public/uploads/Boy-Pics/chico2.jpg'
+import axios from 'axios';
+import { connect } from 'react-redux';
+import {SHOW} from '../../../redux/Types/productType';
+import { useHistory } from 'react-router-dom';
 
-const Boy = () => {
+
+const Boy = (props) => {
+
+    useEffect(()=>{
+        
+        getImg();
+        // eslint-disable-next-line
+    },[])
+
+    const [products, setProducts] = useState ({
+
+        listProducts : []
+    })
+
+    const history = useHistory();
+
+    const getImg = async () => {
+
+        const imgCollection = await axios.get('http://localhost:3000/products')
+        setProducts({
+            ...products, listProducts: imgCollection.data
+        })
+    }
+
+    const boys = products.listProducts.filter(objecProduct => objecProduct.category.includes('Boy'));
+    
+    const saveProduct = (boy) => {
+        props.dispatch({type: SHOW, payload: boy})
+        setTimeout(() => {history.push('/show-product')}, 100);
+    }
+
     return (
         <div className="boyContainer">
             <Navbar/>
             <div className="spaceUnderNav"></div>
             <div className="imgBoyContainer">
-                {/* <img className="boyPic" alt="chico1" src={chico1}></img>
-                <img className="boyPic" alt="chico2" src={chico2}></img> */}
+            {
+                   boys.map(boy => {
+                       return(
+                           <div className="productsData" onClick={() => saveProduct({boy})}  key={products._id}>
+                               <div>
+                                    <img className="imgBoy" src={boy.picture}></img>
+                                    <div className="imgTitle">{boy.name}</div>
+                               </div>
+                           </div>
+                       )
+                   })
+            }
             </div>
         </div>
     )
 }
 
-export default Boy
+const mapStateToProps = state => {
+    return {
+        getProduct : state.productReducer.getProduct
+    }
+};
+
+
+export default connect(mapStateToProps)(Boy)
