b25f22814f54f599a575191284440aebc1d1347a
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-11 09:50:56 +0200

    Add history push to Girl container

--- app/src/containers/Categories/Girl/Girl.jsx
+++ app/src/containers/Categories/Girl/Girl.jsx
@@ -3,7 +3,8 @@ import './Girl.css';
 import Navbar from '../../../components/Navbar/Navbar';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {SHOW} from '../../../redux/Types/productType'
+import {SHOW} from '../../../redux/Types/cartType'
+import { useHistory } from 'react-router-dom';
 
 
 const Girl = (props) => {
@@ -19,6 +20,9 @@ const Girl = (props) => {
         listProducts : []
     })
 
+    const history = useHistory();
+
+
     const getImg = async () => {
 
         const imgCollection = await axios.get('http://localhost:3000/products')
@@ -30,8 +34,9 @@ const Girl = (props) => {
 
     const girl = products.listProducts.filter(objecProduct => objecProduct.category.includes('Girl'));
 
-    const saveProduct = () => {
-        props.dispatch({type: SHOW,  saveProduct})
+    const saveProduct = (product) => {
+        props.dispatch({type: SHOW, payload: product})
+        setTimeout(() => {history.push('/cart')}, 100);
     }
 
     
@@ -43,21 +48,34 @@ const Girl = (props) => {
             <div className="boyContainer">
                 <div className="imgBoyContainer">
                 {
-                    girl.map(productTitle => {
-                        return(
-                            <div className="productsData" key={products._id}>
-                                <div>
-                                        <img className="imgBoy" src={productTitle.picture}></img>
-                                        <div className="imgTitle">{productTitle.name}</div>
-                                </div>
+                girl.map(product => {
+                    return(
+                        <div className="productsData"  key={product._id}>
+                            <div className="cartContainer">
+                                <div className="insideCart">
+                                    <img className="imgBoy" src={product.picture}></img>
+                                    <div className="imgTitle">{product.name}</div>
+                                    <div className="addToCartButton" 
+                                    onClick={() => saveProduct({product})} 
+                                    className="btn btn-primary">
+                                        Add to Cart
+                                    </div>
+                                </div>   
                             </div>
-                        )
-                    })
-                }
+                        </div>
+                    )
+                })
+            }
                 </div>
             </div>
         </div>
     )
 }
 
-export default Girl
+const mapStateToProps = state => {
+    return {
+        cartProduct : state.cartReducer.cartProduct
+    }
+};
+
+export default connect(mapStateToProps)(Girl)
