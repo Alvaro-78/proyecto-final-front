02e3033ddb4a64f4449490764f89b457e798afee
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-11 09:50:30 +0200

    Add select items to cart from jewelry container

--- app/src/containers/Categories/Jewelry/Jewelry.jsx
+++ app/src/containers/Categories/Jewelry/Jewelry.jsx
@@ -2,8 +2,9 @@ import React, {useState , useEffect } from 'react';
 import './Jewelry.css';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {SHOW} from '../../../redux/Types/productType';
+import {SHOW} from '../../../redux/Types/cartType';
 import Navbar from '../../../components/Navbar/Navbar';
+import { useHistory } from 'react-router-dom';
 
 
 
@@ -20,6 +21,9 @@ const Jewelry = (props) => {
         listProducts : []
     })
 
+    const history = useHistory();
+
+
     const getImg = async () => {
 
         const imgCollection = await axios.get('http://localhost:3000/products')
@@ -31,8 +35,9 @@ const Jewelry = (props) => {
 
     const jewelry = products.listProducts.filter(objecProduct => objecProduct.category.includes('Jewelry'));
 
-    const saveProduct = () => {
-        props.dispatch({type: SHOW,  saveProduct})
+    const saveProduct = (product) => {
+        props.dispatch({type: SHOW, payload: product})
+        setTimeout(() => {history.push('/cart')}, 100);
     }
 
     return (
@@ -40,19 +45,26 @@ const Jewelry = (props) => {
         <Navbar/>
         <div className="spaceUnderNav"></div>
         <div className="boyContainer"></div>
-            <div className="imgJewelryContainer" onClick={() => saveProduct()}>
-                {
-                    jewelry.map(productTitle => {
-                        return(
-                            <div className="productsData" key={products._id}>
-                                <div>
-                                    <img className="imgBoy" src={productTitle.picture}></img>
-                                    <div className="imgTitle">{productTitle.name}</div>
-                                </div>
+        <div className="imgBoyContainer">
+            {
+                jewelry.map(product => {
+                    return(
+                        <div className="productsData"  key={product._id}>
+                            <div className="cartContainer">
+                                <div className="insideCart">
+                                    <img className="imgBoy" src={product.picture}></img>
+                                    <div className="imgTitle">{product.name}</div>
+                                    <div className="addToCartButton" 
+                                    onClick={() => saveProduct({product})} 
+                                    className="btn btn-primary">
+                                        Add to Cart
+                                    </div>
+                                    </div>   
                             </div>
-                        )
-                    })
-                }
+                        </div>
+                    )
+                })
+            }
             </div>
         </div>
  )
@@ -61,7 +73,7 @@ const Jewelry = (props) => {
 
 const mapStateToProps = state => {
     return {
-        showProduct : state.productReducer.showProduct
+        cartProduct : state.cartReducer.cartProduct
     }
 };
 
--- app/src/containers/Categories/Shoes/Shoes.jsx
+++ app/src/containers/Categories/Shoes/Shoes.jsx
@@ -3,7 +3,8 @@ import './Shoes.css';
 import Navbar from '../../../components/Navbar/Navbar';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {SHOW} from '../../../redux/Types/productType'
+import {SHOW} from '../../../redux/Types/cartType'
+import { useHistory } from 'react-router-dom';
 
 
 
@@ -20,6 +21,9 @@ const Shoes = (props) => {
         listProducts : []
     })
 
+    const history = useHistory();
+
+
     const getImg = async () => {
 
         const imgCollection = await axios.get('http://localhost:3000/products')
@@ -31,8 +35,9 @@ const Shoes = (props) => {
 
     const shoes = products.listProducts.filter(objecProduct => objecProduct.category.includes('Shoes'));
 
-    const saveProduct = () => {
-        props.dispatch({type: SHOW,  saveProduct})
+    const saveProduct = (product) => {
+        props.dispatch({type: SHOW, payload: product})
+        setTimeout(() => {history.push('/cart')}, 100);
     }
 
     console.log(products)
@@ -41,29 +46,34 @@ const Shoes = (props) => {
         <div>
            <Navbar/>
            <div className="spaceUnderNav"></div>
-           <div className="boyContainer">
-                <div className="imgAccsContainer" onClick={() => saveProduct()}>
-                    {
-                        shoes.map(productTitle => {
-                            return(
-                                <div className="productsData" key={products._id}>
-                                    <div>
-                                            <img className="imgBoy" src={productTitle.picture}></img>
-                                            <div className="imgTitle">{productTitle.name}</div>
-                                    </div>
+           <div className="imgBoyContainer">
+                {
+                    shoes.map(product => {
+                        return(
+                            <div className="productsData"  key={product._id}>
+                                <div className="cartContainer">
+                                    <div className="insideCart">
+                                        <img className="imgBoy" src={product.picture}></img>
+                                        <div className="imgTitle">{product.name}</div>
+                                        <div className="addToCartButton" 
+                                        onClick={() => saveProduct({product})} 
+                                        className="btn btn-primary">
+                                            Add to Cart
+                                        </div>
+                                        </div>   
                                 </div>
-                            )
-                        })
-                    }
-                </div>
-           </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
 
 const mapStateToProps = state => {
     return {
-        showProduct : state.productReducer.showProduct
+        cartProduct : state.cartReducer.cartProduct
     }
 };
 
